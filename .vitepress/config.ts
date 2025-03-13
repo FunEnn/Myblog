5b5b997dad919add8b58a4e709d821e264c9a329
FunEnn <3095852337@qq.com>
2025-03-13 15:20:11 +0800

    refactor: live2d切换为v1.0.3版本

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -47,7 +47,7 @@ async function config() {
       [
         "script",
         {
-          src: "https://fastly.jsdelivr.net/gh/FunEnn/live2d-widget@v1.0.2/dist/autoload.js",
+          src: "https://fastly.jsdelivr.net/gh/FunEnn/live2d-widget@v1.0.3/dist/autoload.js",
           async: true,
           defer: true
         }
